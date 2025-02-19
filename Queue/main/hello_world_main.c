66b47048a8153b72108a783797a95980cec44fcb
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2025-02-18 22:32:52 -0500

    queue - example 2

--- Queue/main/hello_world_main.c
+++ Queue/main/hello_world_main.c
@@ -14,9 +14,18 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 #include "esp_log.h"
+#include "string.h"
 
-
+#define example 2
 /*Defines ----------------------------------------------------------------------------------------------*/
+
+/*Typedefs ---------------------------------------------------------------------------------------------*/
+typedef struct QueueMsg{
+    uint8_t cmd;
+    uint32_t delay_ms;
+    //texto
+    char str[32];
+}QueueMsg_t;
 
 
 /*Global variables -------------------------------------------------------------------------------------*/
@@ -26,6 +35,9 @@ TaskHandle_t taskB;
 static const char *TAG = "main";
 
 QueueHandle_t queue1;
+
+/*Function prototype -----------------------------------------------------------------------------------*/
+BaseType_t buzzer_cmd(uint8_t cmd, uint32_t delay, char *str);
 /*Task entry points ------------------------------------------------------------------------------------*/
 void taskA_entry(void *args);
 void taskB_entry(void *args);
@@ -37,7 +49,12 @@ void app_main(void)
 
 
     /*create queue*/
+#if (example == 1)
     queue1 = xQueueCreate(10/*cantidad de items*/, sizeof(uint8_t)/*cuntos bytes tiene cada item*/);
+#elif (example == 2)
+    queue1 = xQueueCreate(10/*cantidad de items*/, sizeof(QueueMsg_t)/*cuntos bytes tiene cada item*/);
+#endif
+    
     if(queue1 == NULL){
         ESP_LOGE(TAG, "Error al crear el queue");
     } 
@@ -51,10 +68,14 @@ void app_main(void)
         ESP_LOGE(TAG, "Error al crear tarea B");
     }
 
+    //alto el pin
+    //wait 100ms
+    //bajo el pin
     
 }
 
 /*Entry points definitions ----------------------------------------------------------------------------*/
+#if (example == 1)
 void taskA_entry(void *args){
     uint8_t msgCount = 0;
     ESP_LOGI(TAG, "Tarea que envia el queue");
@@ -67,7 +88,7 @@ void taskA_entry(void *args){
         }
 
         msgCount += 1;
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 void taskB_entry(void *args){
@@ -75,7 +96,7 @@ void taskB_entry(void *args){
     ESP_LOGI(TAG, "Tarea que recibe el queue");
     for(;;){
 
-        if(xQueueReceive(queue1, (void *)&msgRcv, pdMS_TO_TICKS(100)) != pdPASS){
+        if(xQueueReceive(queue1, (void *)&msgRcv, portMAX_DELAY) != pdPASS){
             ESP_LOGE(TAG, "Error al recibir el mensaje");
         }else{
             ESP_LOGI(TAG, "MSG: %u", msgRcv);
@@ -84,3 +105,62 @@ void taskB_entry(void *args){
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
+
+#elif (example == 2)
+void taskA_entry(void *args){
+
+    for(;;){
+        buzzer_cmd(1, 500, "on");
+
+        vTaskDelay(pdMS_TO_TICKS(3000));//tarea ingresa a estado bloqueado: no usa el tiempo de la cpu
+    }
+}
+void taskB_entry(void *args){
+    QueueMsg_t msg;
+
+    ESP_LOGI(TAG, "Tarea iniciada");
+    for(;;){
+
+        xQueueReceive(queue1, (void *)&msg, portMAX_DELAY); //bloquea
+        ESP_LOGI(TAG, "\r\n-----------------------------------------------------");
+        ESP_LOGI(TAG, "MSG TEXT: %s", msg.str);
+
+        switch (msg.cmd)
+        {
+        case 0:
+            
+            /* code */
+            break;
+        case 1: //sound
+            //pin en alto
+            ESP_LOGI(TAG, "buzzer on");
+            //delay
+            vTaskDelay(pdMS_TO_TICKS(msg.delay_ms));
+            //pin en bajo
+            ESP_LOGI(TAG, "buzzer off");
+        default:
+            break;
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+/*Function definition -----------------------------------------------------------------------------*/
+BaseType_t buzzer_cmd(uint8_t cmd, uint32_t delay, char *str){
+    BaseType_t ret = 0;
+    QueueMsg_t msg;
+
+    //se define el mensaje
+    msg.cmd = cmd;
+    msg.delay_ms = delay;
+    
+    strcpy(msg.str, str);
+    //se envia el mensaje
+    ret = xQueueSend(queue1, (void *)&msg, portMAX_DELAY);
+    if(ret != pdPASS){
+        ESP_LOGE(TAG, "Error al enviar mensaje a la tarea que maneja el buzzer");
+    }
+
+    return ret;
+}
+#endif
